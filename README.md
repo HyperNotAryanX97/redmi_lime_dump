515916820bbd5ac19bed4c887fb7eaf99fba644e
HyperNotAryanX97 <ahirearyan2016@gmail.com>
2021-07-02 04:16:10 +0000

    Add extras for qssi-user 11 RKQ1.201004.002 21.6.23 release-keys
    Signed-off-by: HyperNotAryanX97 <ahirearyan2016@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## qssi-user 11 RKQ1.201004.002 21.6.23 release-keys
+- Manufacturer: xiaomi
+- Platform: bengal
+- Codename: lime
+- Brand: Redmi
+- Flavor: qssi-user
+- Release Version: 11
+- Id: RKQ1.201004.002
+- Incremental: 21.6.23
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: 440
+- Fingerprint: Redmi/lime_global/lime:10/QKQ1.200830.002/V12.0.4.0.QJQMIXM:user/release-keys
+- OTA version: 
+- Branch: qssi-user-11-RKQ1.201004.002-21.6.23-release-keys
+- Repo: redmi_lime_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cat system/system/priv-app/MiuiGallery/MiuiGallery.apk.* 2>/dev/null >> system/system/priv-app/MiuiGallery/MiuiGallery.apk
+rm -f system/system/priv-app/MiuiGallery/MiuiGallery.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/MiBrowserGlobal/MiBrowserGlobal.apk.* 2>/dev/null >> system/system/priv-app/MiBrowserGlobal/MiBrowserGlobal.apk
+rm -f system/system/priv-app/MiBrowserGlobal/MiBrowserGlobal.apk.* 2>/dev/null
+cat system/system/app/MiuiVideoGlobal/MiuiVideoGlobal.apk.* 2>/dev/null >> system/system/app/MiuiVideoGlobal/MiuiVideoGlobal.apk
+rm -f system/system/app/MiuiVideoGlobal/MiuiVideoGlobal.apk.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null >> vendor/lib64/camera/components/com.qti.node.mialgocontrol.so
+rm -f vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null
+cat cust/app/customized/SuperWallpaperEarth/SuperWallpaperEarth.apk.* 2>/dev/null >> cust/app/customized/SuperWallpaperEarth/SuperWallpaperEarth.apk
+rm -f cust/app/customized/SuperWallpaperEarth/SuperWallpaperEarth.apk.* 2>/dev/null
+cat cust/app/customized/SuperWallpaperSnowmountain/SuperWallpaperSnowmountain.apk.* 2>/dev/null >> cust/app/customized/SuperWallpaperSnowmountain/SuperWallpaperSnowmountain.apk
+rm -f cust/app/customized/SuperWallpaperSnowmountain/SuperWallpaperSnowmountain.apk.* 2>/dev/null
+cat cust/app/customized/SuperWallpaperMars/SuperWallpaperMars.apk.* 2>/dev/null >> cust/app/customized/SuperWallpaperMars/SuperWallpaperMars.apk
+rm -f cust/app/customized/SuperWallpaperMars/SuperWallpaperMars.apk.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
